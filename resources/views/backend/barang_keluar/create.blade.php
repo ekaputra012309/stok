80e5225379355abdb59ce68091f95de2643724b3
ekaputra012309 <eko012309@gmail.com>
2025-10-13 15:55:34 +0700

    remove limit no po

--- resources/views/backend/barang_keluar/create.blade.php
+++ resources/views/backend/barang_keluar/create.blade.php
@@ -40,7 +40,7 @@
                                             <label for="po_number">No PO</label>
                                             <input type="text"
                                                 class="form-control @error('po_number') is-invalid @enderror" id="po_number"
-                                                name="po_number" maxlength="15" placeholder="No PO" required>
+                                                name="po_number" placeholder="No PO" required>
                                             @error('po_number')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
